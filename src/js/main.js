2f265e9fe3940772d888a0d7d0dd454d9a9daaf0
Carlos <73247260+carlosm3@users.noreply.github.com>
2022-11-18 22:04:50 -0700

    Update scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,106 +1,110 @@
-(function () {
-    const transform = document.getElementById("transform");
-    const input = document.getElementById("textInput");
-    const cas = document.getElementById("case-sens");
-    const outputDiv = document.getElementById("output");
+"use strict";
 
-    cas.addEventListener("change", function () {
-        if (cas.checked) {
-            transform.setAttribute("disabled", "disabled");
-        } else {
-            transform.removeAttribute("disabled");
-        }
-    });
+const transform = document.getElementById("transform");
+const input = document.getElementById("textInput");
+const cas = document.getElementById("case-sens");
+const outputDiv = document.getElementById("output");
 
-    transform.addEventListener("change", function (e) {
-        if (e.target.value !== "none") {
-            cas.setAttribute("disabled", "disabled");
-        } else {
-            cas.removeAttribute("disabled");
-        }
-    });
+cas.addEventListener("change", () => {
+    if (cas.checked) {
+        transform.disabled = true;
+        transform.value = "none";
+        transform.style.cursor = "not-allowed";
+    } else {
+        transform.removeAttribute("disabled");
+        transform.style.cursor = "pointer";
+    }
+});
 
-    document.getElementById("submit").addEventListener("click", function () {
-        const startTime = performance.now();
+transform.addEventListener("change", (e) => {
+    if (e.target.value !== "none") {
+        cas.setAttribute("disabled", "disabled");
+    } else {
+        cas.removeAttribute("disabled");
+    }
+});
 
-        const casVal = cas.checked;
-        const inputVal = input.value.split("\n");
-        const inputLen = inputVal.length;
+document.getElementById("submit").addEventListener("click", () => {
+    const startTime = performance.now();
 
-        if (document.getElementById("trim").checked) {
+    const casVal = cas.checked;
+    const inputVal = input.value.split("\n");
+    const inputLen = inputVal.length;
+
+    if (document.getElementById("trim").checked) {
+        inputVal.forEach(function (elem, index) {
+            this[index] = elem.trim();
+        }, inputVal);
+    }
+
+    if (transform.value !== "none") {
+        if (transform.value === "upp") {
             inputVal.forEach(function (elem, index) {
-                this[index] = elem.trim();
+                this[index] = elem.toUpperCase();
+            }, inputVal);
+        } else {
+            inputVal.forEach(function (elem, index) {
+                this[index] = elem.toLowerCase();
             }, inputVal);
         }
+    }
 
-        if (transform.value !== "none") {
-            if (transform.value === "upp") {
-                inputVal.forEach(function (elem, index) {
-                    this[index] = elem.toUpperCase();
-                }, inputVal);
-            } else {
-                inputVal.forEach(function (elem, index) {
-                    this[index] = elem.toLowerCase();
-                }, inputVal);
-            }
+    const hash = {};
+
+    const unique = inputVal.filter(function (item) {
+        if (item === "") return true;
+        else {
+            if (casVal)
+                return hash.hasOwnProperty(item) ? false : (hash[item] = true);
+            else
+                return hash.hasOwnProperty(item.toLowerCase())
+                    ? false
+                    : (hash[item.toLowerCase()] = true);
         }
-
-        const hash = {};
-
-        const unique = inputVal.filter(function (item) {
-            if (item === "") return true;
-            else {
-                if (casVal)
-                    return hash.hasOwnProperty(item)
-                        ? false
-                        : (hash[item] = true);
-                else
-                    return hash.hasOwnProperty(item.toLowerCase())
-                        ? false
-                        : (hash[item.toLowerCase()] = true);
-            }
-        });
-
-        const sort = document.getElementById("sort").value;
-        if (sort !== "none") {
-            if (sort !== "rand") {
-                unique.sort();
-                if (sort === "desc") {
-                    unique.reverse();
-                }
-            } else {
-                let index = unique.length,
-                    randIndex;
-                while (index !== 0) {
-                    randIndex = Math.floor(Math.random() * index);
-                    index--;
-                    [unique[index], unique[randIndex]] = [
-                        unique[randIndex],
-                        unique[index],
-                    ];
-                }
-            }
-        }
-
-        let output;
-        if (document.getElementById("blank").checked) {
-            output = unique.filter(function (item) {
-                if (item !== "") return true;
-            });
-        } else {
-            output = unique;
-        }
-
-        const outputLen = output.length;
-        outputDiv.style.display = "block";
-        document.getElementById("author").scrollIntoView();
-        document.getElementById("summary").style.display = "block";
-        document.getElementById("original").textContent = inputLen.toString();
-        document.getElementById("removed").textContent = (
-            inputLen - outputLen
-        ).toString();
-        document.getElementById("remaining").textContent = outputLen.toString();
-        document.getElementById("textOutput").value = output.join("\n");
-        document.getElementById("time").textContent = ((performance.now() - startTime) / 1000).toFixed(3);
     });
-})();
+
+    const sort = document.getElementById("sort").value;
+    if (sort !== "none") {
+        if (sort !== "rand") {
+            unique.sort();
+            if (sort === "desc") {
+                unique.reverse();
+            }
+        } else {
+            let index = unique.length,
+                randIndex;
+            while (index !== 0) {
+                randIndex = Math.floor(Math.random() * index);
+                index--;
+                [unique[index], unique[randIndex]] = [
+                    unique[randIndex],
+                    unique[index],
+                ];
+            }
+        }
+    }
+
+    let output;
+    if (document.getElementById("blank").checked) {
+        output = unique.filter(function (item) {
+            if (item !== "") return true;
+        });
+    } else {
+        output = unique;
+    }
+
+    const outputLen = output.length;
+    outputDiv.style.display = "block";
+    window.scrollTo(0, document.body.scrollHeight);
+    document.getElementById("summary").style.display = "block";
+    document.getElementById("original").textContent = inputLen.toString();
+    document.getElementById("removed").textContent = (
+        inputLen - outputLen
+    ).toString();
+    document.getElementById("remaining").textContent = outputLen.toString();
+    document.getElementById("textOutput").value = output.join("\n");
+    document.getElementById("time").textContent = (
+        (performance.now() - startTime) /
+        1000
+    ).toFixed(3);
+});
